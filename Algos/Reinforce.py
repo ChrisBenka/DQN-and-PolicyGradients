bb6fd3575adbeb7ac63c5c1b7d517234bb9af1bc
Chris Benka <chrisbenka@gmail.com>
2021-02-01 20:35:04 -0600

    Reinforce

--- Algos/Reinforce.py
+++ Algos/Reinforce.py
@@ -1,108 +1,160 @@
+import gym
+import numpy as np
 import tensorflow as tf
+import tqdm
+from matplotlib import pyplot as plt
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
-from PolicyGradients.Utils.EnvUtils import tf_env_step
-from typing import Tuple
-import numpy as np
-
-eps = np.finfo(np.float32).eps.item()
-
-
-def run_episode(
-    env,
-    initial_state: tf.Tensor,
-    policy: tf.keras.Model,
-    max_steps: int
-) -> Tuple[tf.Tensor, tf.Tensor]:
-    action_probs = tf.TensorArray(tf.float32, 0, True)
-    rewards = tf.TensorArray(tf.int32, 0, True)
-
-    initial_state_shape = initial_state.shape
-    state = initial_state
-
-    for t in tf.range(max_steps):
-        state = tf.expand_dims(state, 0)
-
-        action_logits_t = policy(state)
-
-        action = tf.random.categorical(action_logits_t, 1)[0, 0]
-        action_probs_t = tf.nn.softmax(action_logits_t)
-
-        action_probs = action_probs.write(t, action_probs_t[0, action])
-
-        state, reward, done = tf_env_step(env, action)
-
-        state.set_shape(initial_state_shape)
-
-        rewards = rewards.write(t, reward)
-
-        if tf.cast(done, tf.bool):
-            break
-
-    action_probs = action_probs.stack()
-    rewards = rewards.stack()
-
-    return action_probs, rewards
-
-
-def get_expected_return(rewards: tf.Tensor, gamma: float,
-                        standardize: bool = True) -> tf.Tensor:
-    n = tf.shape(rewards)[0]
-    returns = tf.TensorArray(dtype=tf.float32, size=n)
-    rewards = tf.cast(rewards[::-1], dtype=tf.float32)
-    discounted_sum = tf.constant(0.0)
-    discounted_sum_shape = discounted_sum.shape
-    for i in tf.range(n):
-        reward = rewards[i]
-        discounted_sum = reward + gamma * discounted_sum
-        discounted_sum.set_shape(discounted_sum_shape)
-        returns = returns.write(i, discounted_sum)
-    returns = returns.stack()[::-1]
-
-    if standardize:
-        returns = ((returns - tf.math.reduce_mean(returns)) /
-                   (tf.math.reduce_std(returns) + eps))
-    return returns
-
-
-def compute_loss(action_probs: tf.Tensor, returns: tf.Tensor) -> tf.Tensor:
-    action_log_probs = tf.math.log(action_probs)
-    loss = -tf.math.reduce_sum(action_log_probs * returns)
-    return loss
 
 
 class Policy(tf.keras.Model):
-    def __init__(self, n_actions: int):
+    def __init__(self, n_actions, hidden_units):
         super(Policy, self).__init__(name='Policy')
-        self.fc1 = Dense(6, activation='relu')
+        self.fc1 = Dense(hidden_units, activation='relu')
         self.fc2 = Dense(n_actions)
 
-    def call(self, inputs: tf.Tensor):
+    @tf.function
+    def call(self, inputs, training=None, mask=None):
         x = self.fc1(inputs)
         action_logits = self.fc2(x)
         return action_logits
 
 
+def compute_discounted_rewards(rewards, gamma, normalize_rewards=True):
+    discounted_rewards = []
+    gammas = tf.stack([tf.pow(tf.cast(gamma, tf.float32), tf.cast(i, tf.float32)) for i in tf.range(tf.size(rewards))])
+    for step in tf.range(tf.size(rewards)):
+        discounted_reward = tf.reduce_sum(rewards * gammas) if step == 0 else tf.reduce_sum(
+            rewards[:-step] * gammas[:-step])
+        discounted_rewards.append(discounted_reward)
+    discounted_rewards = tf.stack(discounted_rewards)
+    if normalize_rewards:
+        discounted_rewards = (discounted_rewards - tf.math.reduce_mean(discounted_rewards)) / \
+                             tf.math.reduce_sum(tf.math.reduce_std(discounted_rewards))
+    return discounted_rewards
+
+
+def _compute_loss(action_probs, discounted_rewards):
+    log_action_probs = tf.math.log(action_probs)
+    return -tf.reduce_sum(log_action_probs * discounted_rewards)
+
+
 class Reinforce:
-    def __init__(self, n_actions, lr=.001):
-        self.policy = Policy(n_actions)
-        self.optim = Adam(learning_rate=lr)
+    def __init__(self, env, lr, policy, gamma, max_episodes, max_eps_steps, normalize_rewards=True,
+                 reward_threshold=195, display_progress_intvl=250):
+        self.env = env
+        self.lr = lr
+        self.policy = policy
+        self.policy_optimizer = Adam(learning_rate=lr)
+        self.max_episodes = max_episodes
+        self.max_eps_steps = max_eps_steps
+        self.gamma = gamma
+        self.normalize_rewards = normalize_rewards
+        self.reward_threshold = reward_threshold
+        self.display_progress_intvl = display_progress_intvl
 
-    @tf.function
-    def train_step(self, env, initial_state: tf.Tensor, gamma: float,
-                   max_steps_per_episode: int) -> tf.Tensor:
+        self.steps_taken = 0
+
+    def env_step(self, action):
+        state, reward, done, _ = self.env.step(action)
+        return (
+            state.astype(np.float32),
+            np.array(reward, np.float32),
+            np.array(done, np.int)
+        )
+
+    def tf_env_step(self, action: tf.Tensor):
+        return tf.numpy_function(self.env_step, [action], [tf.float32, tf.float32, tf.uint8])
+
+    def _run_episode(self):
+        state = tf.constant(self.env.reset(), dtype=tf.float32)
+        rewards = tf.TensorArray(tf.float32, 0, True)
+        action_probs = tf.TensorArray(tf.float32, 0, True)
+
+        state_shape = state.shape
+
+        for step in tf.range(self.max_eps_steps):
+            action, action_logits_step = self.get_action(state)
+            action_probs_step = tf.nn.softmax(action_logits_step)[0, action]
+            state, reward, done = self.tf_env_step(action)
+
+            self.steps_taken += 1
+
+            action_probs = action_probs.write(step, action_probs_step)
+            rewards = rewards.write(step, reward)
+
+            state.set_shape(state_shape)
+
+            if tf.cast(done, tf.bool):
+                break
+
+        return action_probs.stack(), rewards.stack()
+
+    def get_action(self, state):
+        action_logits_step = self.policy(tf.expand_dims(state, 0))
+        action = tf.squeeze(tf.random.categorical(logits=action_logits_step, num_samples=1))
+        return action, action_logits_step
+
+    def __call__(self, *args, **kwargs):
+        running_reward = 0
+        running_rewards, episode_rewards = [], []
+        with tqdm.trange(1, self.max_episodes) as t:
+            for episode in t:
+                rewards = self.train()
+
+                episode_reward = float(tf.reduce_sum(rewards))
+                running_reward = .99 * running_reward + .01 * episode_reward
+
+                running_rewards.append(running_reward)
+                episode_rewards.append(episode_reward)
+
+                t.set_description(f"Episode {episode}")
+                t.set_postfix(episode_reward=episode_reward, running_reward=running_reward)
+
+                if running_reward >= self.reward_threshold:
+                    print(
+                        f"Solved at episode: {episode} mean, mean_reward: {running_reward}, steps_taken={self.steps_taken}")
+                    break
+        return running_rewards, episode_rewards
+
+    def train(self):
         with tf.GradientTape() as tape:
-            action_probs, rewards = run_episode(env, initial_state, gamma,
-                                                max_steps_per_episode)
+            action_probs, rewards = self._run_episode()
+            discounted_rewards = compute_discounted_rewards(rewards, self.gamma)
+            loss = _compute_loss(action_probs, discounted_rewards)
+        grads = tape.gradient(loss, self.policy.trainable_variables)
+        self.policy_optimizer.apply_gradients(zip(grads, self.policy.trainable_variables))
+        return rewards
 
-            returns = get_expected_return(rewards, gamma)
+    def demo(self):
+        state = tf.constant(env.reset(), dtype=tf.float32)
+        episode_reward = 0
+        for _ in tf.range(self.max_eps_steps):
+            action, _ = self.get_action(state)
+            state, reward, done = self.tf_env_step(action)
+            episode_reward += float(reward)
+            env.render()
+            if done:
+                print(f"demo episode reward: {episode_reward}")
+                return
 
-            action_probs, returns = [
-                tf.expand_dims(x, 1) for x in [action_probs, returns]
-            ]
 
-          loss = compute_loss(action_probs, returns)
-        grads = tape.gradient(loss,self.policy.trainable_variables)
-        self.optim.apply_gradients(zip(loss,self.policy.trainable_variables))
-        episode_reward = tf.math.reduce_sum(rewards)
-        return episode_reward
+if __name__ == '__main__':
+    env = gym.make('CartPole-v0')
+
+    seed = 19
+    env.seed(seed)
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
+
+    policy = Policy(env.action_space.n, hidden_units=12)
+    reinforce = Reinforce(env, lr=.0025, policy=policy, gamma=.99, max_episodes=10000, max_eps_steps=250,
+                          reward_threshold=195)
+    running_rewards, episode_rewards = reinforce()
+    episodes = np.arange(len(running_rewards))
+    plt.plot(episodes, running_rewards, label='running_reward')
+    plt.plot(episodes, episode_rewards, label='episode_reward')
+    plt.legend()
+    plt.show()
+
+    reinforce.demo()
